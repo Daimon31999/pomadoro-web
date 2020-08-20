140a59f148e7cdaeddb6df7014a55014426282f9
Dima Hinev <dimahinev31999@gmail.com>
2020-08-20 09:17:00 +0300

    hover on Ipad fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 export default function header(props) {
   return (
-    <div className="flex flex-row justify-between md:px-40 sm:px-10 text-white p-2 mb-5 text-2xl text-center font-futura-bold">
+    <div className="flex flex-row justify-between lg:px-40 sm:px-10 text-white p-2 mb-5 text-2xl text-center font-futura-bold">
       <p className="cursor-pointer hover:text-gray-200 ml-10">Settings</p>
       <p
         className="cursor-pointer hover:text-gray-200 mr-10"
